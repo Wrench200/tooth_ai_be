88d79e298464a984898923ac4494fd2695f1ccd2
Tebi Njeik <117078165+Wrench200@users.noreply.github.com>
2025-06-14 05:15:01 +0000

    addded new secret server address

--- setup.py
+++ setup.py
@@ -5,4 +5,5 @@ import os
 # Set your API token (make sure this is securely stored in production)
 api_token = os.getenv("REPLICATE_API_TOKEN")  # Assumes it's already set in the environment
 # api_token = os.environ['replicate_api_token']
-server_address = "https://9bf05f2c-b1e2-4e37-b646-c04641bbf035-00-7bpcefl3zkr2.riker.replit.dev"
+# server_address = os.environ['server_address']
+server_address = os.getenv('SERVER_ADDRESS')
