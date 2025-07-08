f05b7f672e3f04615d1c1ca299a2fb562ed258fd
wrench200 <tnjeik@gmail.com>
2025-07-08 15:59:09 +0100

    edited setup

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 # Set your API token (make sure this is securely stored in production)
-# api_token = os.getenv("REPLICATE_API_TOKEN")  # Assumes it's already set in the environment
-api_token = "r8_AVRihwc69CfEIKMWdhBQesMJmIXzfU80eaII2"
+api_token = os.getenv("REPLICATE_API_TOKEN")  # Assumes it's already set in the environment
+# api_token = "r8_AVRihwc69CfEIKMWdhBQesMJmIXzfU80eaII2"
 server_address = "http://127.0.0.1:8080"
